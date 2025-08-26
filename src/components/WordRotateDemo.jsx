40903eee6c21a829de465835c1f560dcccd3aced
SachinPa30 <patilsachin762002@gmail.com>
2025-08-26 15:34:24 +0530

    Update WordRotateDemo.jsx

--- src/components/WordRotateDemo.jsx
+++ src/components/WordRotateDemo.jsx
@@ -1,6 +1,6 @@
 import {WordRotate} from "../components/ui/WordRotate";
 
-export function WordRotateDemo() {
+export default function  WordRotateDemo() {
   return (
     <WordRotate
       className="text-4xl font-bold text-white dark:text-white"
